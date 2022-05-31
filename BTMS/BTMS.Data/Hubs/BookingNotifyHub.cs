be010e049a3eff2becf9151399f101a4f88bdd59
coderrakib1 <mdrakibulislamcse50@gmail.com>
2022-05-31 22:15:13 +0600

    first comit

---
+++ BTMS/BTMS.Data/Hubs/BookingNotifyHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace BTMS.Data.Hubs
+{
+    public class BookingNotifyHub:Hub
+    {
+        public async Task BookingNotify(BookingMessage message)
+        {
+            await Clients.All.SendAsync("seatBooked", message);
+        }
+    }
+}
